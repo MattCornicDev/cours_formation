e3a15b295e76268b6312f4327c9fed8b2d558fc9
MattCornicDev <glgharib@gmail.com>
2022-11-27 00:03:06 +0100

    modif

--- src/components/affichageCours/AffichageCours.jsx
+++ src/components/affichageCours/AffichageCours.jsx
@@ -12,7 +12,7 @@ export default class AffichageCours extends Component {
         return (
             <>
                 <Container>
-                    <Row xs={1} md={2} sm={1} lg={4} >
+                    <Row xs={1} md={3} sm={1} lg={4} >
                         <Link to="/gitCours">
                             <Col>
                                 <Tilt>
@@ -40,33 +40,38 @@ export default class AffichageCours extends Component {
                                 </Tilt>
                             </Col>
                         </Link>
-                        <Col>
-                            <Tilt>
-                                <Card>
-                                    <Card.Img src={process.env.PUBLIC_URL + "/html.svg.png"} alt="logo html" />
-                                    <Card.Body>
-                                        <h5 className="card-title">HTML</h5>
-                                        <p className="card-text">cours HTML.</p>
-                                    </Card.Body>
-                                </Card>
-                            </Tilt>
-                        </Col>
-                        <Col>
-                            <Tilt>
-                                <Card>
-                                    <Card.Img src={process.env.PUBLIC_URL + "/bootstrap_logo.svg.png"} alt="logo bootstrap" />
-                                    <Card.Body>
-                                        <h5 className="card-title">BOOTSTRAP</h5>
-                                        <p className="card-text">Cours bootstrap bientôt.</p>
-                                    </Card.Body>
-                                </Card>
-                            </Tilt>
-                        </Col>
+
+                        <Link>
+                            <Col>
+                                <Tilt>
+                                    <Card>
+                                        <Card.Img src={process.env.PUBLIC_URL + "/html.svg.png"} alt="logo html" />
+                                        <Card.Body>
+                                            <h5 className="card-title">HTML</h5>
+                                            <p className="card-text">cours HTML.</p>
+                                        </Card.Body>
+                                    </Card>
+                                </Tilt>
+                            </Col>
+                        </Link>
+                        <Link>
+                            <Col>
+                                <Tilt>
+                                    <Card>
+                                        <Card.Img src={process.env.PUBLIC_URL + "/bootstrap_logo.svg.png"} alt="logo bootstrap" />
+                                        <Card.Body>
+                                            <h5 className="card-title">BOOTSTRAP</h5>
+                                            <p className="card-text">Cours bootstrap bientôt.</p>
+                                        </Card.Body>
+                                    </Card>
+                                </Tilt>
+                            </Col>
+                        </Link>
                         <Link to="/react_cours">
                             <Col>
                                 <Tilt>
                                     <Card>
-                                        <Card.Img src={process.env.PUBLIC_URL + "logo192.png"} alt="logo bootstrap" />
+                                        <Card.Img src={process.env.PUBLIC_URL + "/logo192.png"} alt="logo bootstrap" />
                                         <Card.Body>
                                             <h5 className="card-title">ReactJS</h5>
                                             <p className="card-text">Cours React.</p>
